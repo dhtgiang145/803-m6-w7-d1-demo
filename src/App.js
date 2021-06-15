731891c93fe3a1f2c4d2e891c28d8f6a0542a699
Giang Do <dhtgiang145@gmail.com>
2021-06-14 20:35:16 -0700

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import BarChart from "./barchart/BarChart";
+import Example from "./spheres/index";
+import TreeApp from "./tree/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>Redux Animation</p>
+        </header>
+        <button>
+          <a
+            className="barchart"
+            href="/barchart"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bar Chart
+          </a>
+        </button>
+        <button>
+          <a
+            className="barchart"
+            href="/spheres"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sphere Animation
+          </a>
+        </button>
+        <button>
+          <a
+            className="barchart"
+            href="/thirdanimation"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Tree Animation
+          </a>
+        </button>
+        <Switch>
+          <Route exact path="/barchart">
+            <BarChart />
+          </Route>
+          <Route exact path="/spheres">
+            <Example />
+          </Route>
+          <Route exact path="/tree">
+            <TreeApp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
